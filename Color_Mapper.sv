1813a7bc33140b87fb9b7044222d02cf35c6b651
Caleb Chow <calebchow9@gmail.com>
2021-11-15 23:55:08 -0600

    updated with spritesheet

--- Color_Mapper.sv
+++ Color_Mapper.sv
@@ -12,29 +12,17 @@
 //    University of Illinois ECE Department                              --
 //-------------------------------------------------------------------------
 
-
 module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
                        output logic [7:0]  Red, Green, Blue );
     
     logic ball_on;
-	 
- /* Old Ball: Generated square box by checking if the current pixel is within a square of length
-    2*Ball_Size, centered at (BallX, BallY).  Note that this requires unsigned comparisons.
-	 
-    if ((DrawX >= BallX - Ball_size) &&
-       (DrawX <= BallX + Ball_size) &&
-       (DrawY >= BallY - Ball_size) &&
-       (DrawY <= BallY + Ball_size))
-
-     New Ball: Generates (pixelated) circle by using the standard circle formula.  Note that while 
-     this single line is quite powerful descriptively, it causes the synthesis tool to use up three
-     of the 12 available multipliers on the chip!  Since the multiplicants are required to be signed,
-	  we have to first cast them from logic to int (signed by default) before they are multiplied). */
 	  
     int DistX, DistY, Size;
 	 assign DistX = DrawX - BallX;
     assign DistY = DrawY - BallY;
     assign Size = Ball_size;
+	 
+	 logic [15:0] mem_addr_in;
 	  
     always_comb
     begin:Ball_on_proc
@@ -43,15 +31,19 @@ module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
         else 
             ball_on = 1'b0;
      end 
-       
+
+// map dimensions: 224 x 247 = 55328 pixels	  
+		 
     always_comb
     begin:RGB_Display
+	 // draw PacMan
         if ((ball_on == 1'b1)) 
         begin 
             Red = 8'hff;
             Green = 8'h55;
             Blue = 8'h00;
-        end       
+        end
+	 // draw background maze
         else 
         begin 
             Red = 8'h00; 
