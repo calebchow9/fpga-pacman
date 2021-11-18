2c83d449ddda16ebcca9b7db2dd1b6bd23b51472
Caleb Chow <calebyc2@illinois.edu>
2021-11-17 19:41:46 -0600

    some edges working

--- ball.sv
+++ ball.sv
@@ -22,10 +22,10 @@ module  ball ( input Reset, frame_clk,
 	 
     parameter [9:0] Ball_X_Center=202;  // Center position on the X axis
     parameter [9:0] Ball_Y_Center=253;  // Center position on the Y axis
-    parameter [9:0] Ball_X_Min=0;       // Leftmost point on the X axis
-    parameter [9:0] Ball_X_Max=404;     // Rightmost point on the X axis
-    parameter [9:0] Ball_Y_Min=0;       // Topmost point on the Y axis
-    parameter [9:0] Ball_Y_Max=447;     // Bottommost point on the Y axis
+    parameter [9:0] Ball_X_Min=7;       // left border of maze
+    parameter [9:0] Ball_X_Max=396;     // right border of maze
+    parameter [9:0] Ball_Y_Min=7;       // top border of maze
+    parameter [9:0] Ball_Y_Max=440;     // bottom border of maze
     parameter [9:0] Ball_X_Step=1;      // Step size on the X axis
     parameter [9:0] Ball_Y_Step=1;      // Step size on the Y axis
 
@@ -34,91 +34,121 @@ module  ball ( input Reset, frame_clk,
     always_ff @ (posedge Reset or posedge frame_clk )
     begin: Move_Ball
         if (Reset)  // Asynchronous Reset
-        begin 
-            Ball_Y_Motion <= 10'd0; //Ball_Y_Step;
-					Ball_X_Motion <= 10'd0; //Ball_X_Step;
-					Ball_Y_Pos <= Ball_Y_Center;
-					Ball_X_Pos <= Ball_X_Center;
-        end
-           
+				begin 
+					Ball_Y_Motion <= 10'd0; //Ball_Y_Step;
+						Ball_X_Motion <= 10'd0; //Ball_X_Step;
+						Ball_Y_Pos <= Ball_Y_Center;
+						Ball_X_Pos <= Ball_X_Center;
+				end  
         else 
-        begin 
-				 if ( (Ball_Y_Pos + Ball_Size) >= Ball_Y_Max )  // Ball is at the bottom edge, BOUNCE!
-				 begin
-					  Ball_Y_Motion <= (~ (Ball_Y_Step) + 1'b1);  // 2's complement.
-						wall = 1'b1;
-				 end 
-				 else if ( (Ball_Y_Pos - Ball_Size) <= Ball_Y_Min )  // Ball is at the top edge, BOUNCE!
-					begin
-					  Ball_Y_Motion <= Ball_Y_Step;
-					  wall = 1'b1;
-					end
-				  else if ( (Ball_X_Pos + Ball_Size) >= Ball_X_Max )  // Ball is at the Right edge, BOUNCE!
-				  begin
-					  Ball_X_Motion <= (~ (Ball_X_Step) + 1'b1);  // 2's complement.
-					  wall = 1'b1;
-				  end
-					  
-				 else if ( (Ball_X_Pos - Ball_Size) <= Ball_X_Min )  // Ball is at the Left edge, BOUNCE!
-				 begin
-					  Ball_X_Motion <= Ball_X_Step;
-						wall = 1'b1;
-				 end
-					  
-				 else 
-				 begin
-					  Ball_Y_Motion <= Ball_Y_Motion;  // Ball is somewhere in the middle, don't bounce, just keep moving
-					  wall = 1'b0;
-				end
-				
-				if (~wall)
 				begin
 					case (keycode)
+						// LEFT
 						8'h04 : begin
-
-										Ball_X_Motion <= -1;//A
-										Ball_Y_Motion<= 0;
+										Ball_Y_Motion <= 0;
+										// edge checks
+										// left wall
+										if(Ball_X_Pos - Ball_Size <= Ball_X_Min)
+											begin
+												Ball_X_Motion <= 0;
+												Ball_Y_Motion <= 0;
+											end
+										else
+											Ball_X_Motion <= -1;
+										
 								  end
-								  
+						// RIGHT
 						8'h07 : begin
-									
-								  Ball_X_Motion <= 1;//D
 									Ball_Y_Motion <= 0;
+									// edge checks
+									// left wall
+									if(Ball_X_Pos - Ball_Size <= Ball_X_Min)
+										begin
+											Ball_X_Motion <= 0;
+											Ball_Y_Motion <= 0;
+										end
+									// right wall
+									else if(Ball_X_Pos + Ball_Size >= Ball_X_Max)
+										begin
+											Ball_X_Motion <= 0;
+											Ball_Y_Motion <= 0;
+										end
+									// top wall
+									else if(Ball_Y_Pos - Ball_Size <= Ball_Y_Min)
+										begin
+											Ball_Y_Motion <= 0;
+										end
+									else if(Ball_Y_Pos + Ball_Size >= Ball_Y_Max)
+										begin
+											Ball_Y_Motion <= 0;
+										end
+									else
+										Ball_X_Motion <= 1;
 								  end
-
-								  
+						// DOWN		  
 						8'h16 : begin
-
-								  Ball_Y_Motion <= 1;//S
 									Ball_X_Motion <= 0;
+									// edge checks
+									// left wall
+									if(Ball_X_Pos - Ball_Size <= Ball_X_Min)
+										begin
+											Ball_X_Motion <= 0;
+										end
+									// right wall
+									else if(Ball_X_Pos + Ball_Size >= Ball_X_Max)
+										begin
+											Ball_X_Motion <= 0;
+										end
+									// top wall
+									else if(Ball_Y_Pos - Ball_Size <= Ball_Y_Min)
+										begin
+											Ball_X_Motion <= 0;
+											Ball_Y_Motion <= 0;
+										end
+									else if(Ball_Y_Pos + Ball_Size >= Ball_Y_Max)
+										begin
+											Ball_X_Motion <= 0;
+											Ball_Y_Motion <= 0;
+										end
+									else
+										Ball_Y_Motion <= 1;
 								 end
-								  
+						// UP		  
 						8'h1A : begin
-								  Ball_Y_Motion <= -1;//W
 									Ball_X_Motion <= 0;
+									// edge checks
+									// left wall
+									if(Ball_X_Pos - Ball_Size <= Ball_X_Min)
+										begin
+											Ball_X_Motion <= 0;
+										end
+									// right wall
+									else if(Ball_X_Pos + Ball_Size >= Ball_X_Max)
+										begin
+											Ball_X_Motion <= 0;
+										end
+									// top wall
+									else if(Ball_Y_Pos - Ball_Size <= Ball_Y_Min)
+										begin
+											Ball_X_Motion <= 0;
+											Ball_Y_Motion <= 0;
+										end
+									else if(Ball_Y_Pos + Ball_Size >= Ball_Y_Max)
+										begin
+											Ball_X_Motion <= 0;
+											Ball_Y_Motion <= 0;
+										end
+									else
+										Ball_Y_Motion <= -1;
 								 end	  
 						default: ;
 					endcase
+					
+					 Ball_Y_Pos <= (Ball_Y_Pos + Ball_Y_Motion);  // Update ball position
+					 Ball_X_Pos <= (Ball_X_Pos + Ball_X_Motion);
 				end
-				 
-				
-				 
-				 Ball_Y_Pos <= (Ball_Y_Pos + Ball_Y_Motion);  // Update ball position
-				 Ball_X_Pos <= (Ball_X_Pos + Ball_X_Motion);
-			
-			
-	  /**************************************************************************************
-	    ATTENTION! Please answer the following quesiton in your lab report! Points will be allocated for the answers!
-		 Hidden Question #2/2:
-          Note that Ball_Y_Motion in the above statement may have been changed at the same clock edge
-          that is causing the assignment of Ball_Y_pos.  Will the new value of Ball_Y_Motion be used,
-          or the old?  How will this impact behavior of the ball during a bounce, and how might that 
-          interact with a response to a keypress?  Can you fix it?  Give an answer in your Post-Lab.
-      **************************************************************************************/
-      
 			
 		end  
-    end
        
     assign BallX = Ball_X_Pos;
    
--- output_files/lab6.cdf
+++ output_files/lab6.cdf
@@ -4,7 +4,7 @@ JedecChain;
 	DefaultMfr(6E);
 
 	P ActionCode(Cfg)
-		Device PartName(10M50DAF484) Path("C:/Users/caleb/Desktop/School/ece385/lab6/output_files/") File("lab6.sof") MfrSpec(OpMask(1));
+		Device PartName(10M50DAF484) Path("C:/Users/caleb/Desktop/fpga-pacman/output_files/") File("lab6.sof") MfrSpec(OpMask(1));
 
 ChainEnd;
 
